6d8b912fe1c98ddced5f19a5e028617a53b178f4
Shaun Colley <swc7838@gmail.com>
2019-10-16 12:26:54 -0500

    changing algorithm to account for duplicate letters to make sure all letters are same in both strrings

--- alg6.js
+++ alg6.js
@@ -3,20 +3,45 @@
 let test1 = "abcd"
 let test2 = "dbca"
 let test3 = "abce"
+let test4 = "apple"
+let test5 = "elpea"
+let test6 = "elppa"
 
+// write a separte function to return a map of characters and count
+
+const mapString = (string) => {
+  let map = {};
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    if (!map[char]) {
+      map[char] = 1;
+    } else {
+      map[char] = 1 + map[char];
+    }
+  }
+  return map
+}
+
+
+// write function to map maps and perform check in maps
 
 const checkPerm = (s1, s2) => {
-  let hashMap = {};
+
   // checking that input even exists
   if(!s1 || !s2 || s1.length !== s2.length) {
     return false;
   }
 
+  // allocate memory storage if input is legit
+
+  let map1 = mapString(s1);
+  let map2 = mapString(s2)
+
+
   for (let i = 0; i < s1.length; i++) {
-    hashMap[s1[i]] = s1[i];
-  }
-  for (let i = 0; i < s2.length; i++) {
-    if (!hashMap[s2[i]]) {
+    let char = s1[i]
+    if (map1[char] !== map2[char]) {
       return false
     }
   }
@@ -26,6 +51,7 @@ const checkPerm = (s1, s2) => {
 console.log(checkPerm(test1, test2)); // logs true
 console.log(checkPerm(test1, test3)) // logs false
 console.log(checkPerm(test1)) // logs false
-
+console.log(checkPerm(test4, test5)) // logs false
+console.log(checkPerm(test4, test6)) // logs true
 
 //
