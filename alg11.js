788fbb032d1f98e2d3e0863637fdb511cf787775
Shaun Colley <swc7838@gmail.com>
2019-10-29 20:21:35 -0500

    solved another 2 algorithms

--- alg11.js
+++ alg11.js
@@ -25,7 +25,7 @@ const greatestSum = (array, num) => {
   return result;
 }
 
-console.log(greatestSum(testOne, 3))  // return 16 + 7 + 4
+// console.log(greatestSum(testOne, 3))  // return 16 + 7 + 4
 
 // autocomplete class that returns all dictionary words with a prefix
 
@@ -55,4 +55,51 @@ const autoComplete = (input) => {
   return result
 }
 
-console.log(autoComplete('ab'))
+// console.log(autoComplete('ab'))
+
+// next exercise
+
+// swap two integers with out using any variables
+const swap = (x,y) => {
+  x = x + y // add them up
+  y = x - y // y is total minus y
+  x = x - y // x is total minus new y
+  return [x,y]
+}
+
+// console.log(swap(2,3))
+
+// given an array containin g all numbers from 1 to n, expect 2 find missing 2 numbers
+
+// array can be unsorted
+
+testTwo = [4, 2, 3]
+
+const findMissingTwo = (array) => {
+  let one;
+  let two;
+
+  // first let's find max possible number, this will be length + 2
+  let n = array.length + 2
+
+  // now let's map all the values in the array
+  let numbers = {}
+  for (let i = 0; i < array.length; i++) {
+    numbers[array[i]] = array[i];
+  }
+
+  // now if element is missing from 1 to n, then it will be added to numbers
+  for (let i = 1; i < n + 1; i++) {
+    if (!numbers[i] && !one) {
+      one = i
+    } else if (!numbers[i]) {
+      two = i
+    }
+  }
+
+
+  return `${one}, ${two}`
+}
+
+
+console.log(findMissingTwo(testTwo))
